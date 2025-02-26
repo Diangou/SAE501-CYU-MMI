67797afde7e513cb4d7a14e46b8a0e902c0a9ad7
Diangou <diangou.cmr09@gmail.com>
2025-02-26 10:30:39 +0100

    404 en cours

--- server/front-end-router.js
+++ server/front-end-router.js
@@ -129,11 +129,9 @@ router.get('/auteur-details/:id', async (req, res) => {
     }
 });
 
-router.use((req, res) => {
-    res.status(404).render("pages/front-end/404.njk", {
-        title: "Page non trouvée",
-    });
-});
+
+
+
 
 
 
